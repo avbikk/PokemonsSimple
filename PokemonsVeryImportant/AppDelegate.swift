d3dff0ae4372bdb1a62881e2c8d59adbd36ad068
Alsu Bikkulova <av.bikkulova@gmail.com>
2020-01-24 13:07:16 +0300

    Deleted scene

--- PokemonsVeryImportant/AppDelegate.swift
+++ PokemonsVeryImportant/AppDelegate.swift
@@ -11,7 +11,17 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow()
+        let pokemonsInitialDataViewController = Assembly.configure()
+        let navigationController = UINavigationController(rootViewController: pokemonsInitialDataViewController)
+        window?.rootViewController = navigationController
+        pokemonsInitialDataViewController.view.backgroundColor = .red
+        window?.makeKeyAndVisible()
+
         return true
     }
 }
